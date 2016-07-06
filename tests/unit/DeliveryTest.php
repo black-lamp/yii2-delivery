0067fdebadb9dc1896a559656159a9c1186e7f6f
RuslanSaiko <ruslan.saiko.dev@gmail.com>
2016-07-06 11:13:36 +0300

    add test

---
+++ tests/unit/DeliveryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class DeliveryTest extends \yii\codeception\TestCase
+{
+    public $appConfig = '@tests/config/unit.php';
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+}
